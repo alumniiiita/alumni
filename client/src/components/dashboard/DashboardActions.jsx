406ff4d7734c54a6bdb9759b5914cbeef51e733e
alumniiiita <alumni.office@iiita.ac.in>
2025-04-26 15:25:25 +0530

    removed delete account

--- client/src/components/dashboard/DashboardActions.jsx
+++ client/src/components/dashboard/DashboardActions.jsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteAccount } from "../../actions/users";
 
-const DashboardActions = ({ deleteAccount, auth: { user } }) => {
+const DashboardActions = ({ deleteAccount }) => {
 	return (
 		<div className="my-3 mx-4">
 			<Link
@@ -12,7 +12,10 @@ const DashboardActions = ({ deleteAccount, auth: { user } }) => {
 				className="btn btn-light"
 				style={{ width: "100%", marginBottom: "1em" }}
 			>
-				<i className="fas fa-user-circle" style={{ marginRight: "0.5em" }}></i>
+				<i
+					className="fas fa-user-circle"
+					style={{ marginRight: "0.5em" }}
+				></i>
 				Edit Profile
 			</Link>
 			<Link
@@ -20,7 +23,10 @@ const DashboardActions = ({ deleteAccount, auth: { user } }) => {
 				className="btn btn-light"
 				style={{ width: "100%", marginBottom: "1em" }}
 			>
-				<i className="fab fa-black-tie" style={{ marginRight: "0.5em" }}></i>
+				<i
+					className="fab fa-black-tie"
+					style={{ marginRight: "0.5em" }}
+				></i>
 				Add Experience
 			</Link>
 			<Link
@@ -28,7 +34,10 @@ const DashboardActions = ({ deleteAccount, auth: { user } }) => {
 				className="btn btn-light"
 				style={{ width: "100%", marginBottom: "1em" }}
 			>
-				<i className="fas fa-edit" style={{ marginRight: "0.5em" }}></i>
+				<i
+					className="fas fa-edit"
+					style={{ marginRight: "0.5em" }}
+				></i>
 				Create Post
 			</Link>
 			<Link
@@ -36,32 +45,32 @@ const DashboardActions = ({ deleteAccount, auth: { user } }) => {
 				className="btn btn-light"
 				style={{ width: "100%", marginBottom: "1em" }}
 			>
-				<i className="fas fa-graduation-cap" style={{ marginRight: "0.5em" }}></i>
+				<i
+					className="fas fa-graduation-cap"
+					style={{ marginRight: "0.5em" }}
+				></i>
 				Add Education
 			</Link>
-
-			{user && (user.role === "Admin" || user.adminType === "head") && (
-				<Link
-					to=""
-					onClick={() => {
-						deleteAccount();
-					}}
-					className="btn btn-danger"
-					style={{ width: "100%" }}
-				>
-					<i className="fas fa-user-minus" style={{ marginRight: "0.5em" }}></i>
-					Delete Account
-				</Link>
-			)}
+			{/* <Link
+				to=""
+				onClick={() => {
+					deleteAccount();
+				}}
+				className="btn btn-danger"
+				style={{ width: "100%" }}
+			>
+				<i
+					className="fas fa-user-minus"
+					style={{ marginRight: "0.5em" }}
+				></i>
+				Delete Account
+			</Link> */}
 		</div>
 	);
 };
 
 DashboardActions.propTypes = {
 	deleteAccount: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired,
 };
 
-export default connect(state => ({
-	auth: state.auth
-}), { deleteAccount })(DashboardActions);
+export default connect(null, { deleteAccount })(DashboardActions);
