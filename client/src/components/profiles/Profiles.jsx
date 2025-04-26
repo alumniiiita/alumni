fa1cb6dc11e1c3c97f03a5a521b05f3ab16cae2d
alumniiiita <alumni.office@iiita.ac.in>
2025-04-26 17:55:59 +0530

    revised the search

--- client/src/components/profiles/Profiles.jsx
+++ client/src/components/profiles/Profiles.jsx
@@ -4,11 +4,16 @@ import { connect } from "react-redux";
 import Spinner from "../layouts/Spinner";
 import UserCard from "./UserCard";
 import { useLocation } from "react-router-dom";
-import { getUsersByType } from "../../actions/users";
+import { getUsers, getUsersByType } from "../../actions/users";
 import { closeSideNav } from "../../actions/alert";
 import UsersByType from "./UsersByType";
 
+function useQuery() {
+	return new URLSearchParams(useLocation().search);
+}
+
 const Profiles = ({
+	getUsers,
 	closeSideNav,
 	getUsersByType,
 	user: { users, loading },
@@ -17,40 +22,37 @@ const Profiles = ({
 	const [faculty, setFaculty] = useState([]);
 	const [alumni, setAlumni] = useState([]);
 	const [admins, setAdmins] = useState([]);
+
+	const query = useQuery();
+	const searchQuery = query.get("search");
 	const [search, setSearch] = useState("");
-	const [allUsers, setAllUsers] = useState([]); // store all users locally
+
+	useEffect(async () => {
+		closeSideNav();
+		const students = await getUsersByType("student");
+		setStudents(students);
+
+		const alumnis = await getUsersByType("alumni");
+		setAlumni(alumnis);
+
+		const faculty = await getUsersByType("faculty");
+		setFaculty(faculty);
+
+		const admins = await getUsersByType("admin");
+		setAdmins(admins);
+	}, []);
 
 	useEffect(() => {
-		closeSideNav();
+		getUsers(searchQuery);
+	}, []);
 
-		const loadUsers = async () => {
-			const studentsData = await getUsersByType("student");
-			const alumniData = await getUsersByType("alumni");
-			const facultyData = await getUsersByType("faculty");
-			const adminsData = await getUsersByType("admin");
+	const handleSubmit = () => {
+		if(search.trim()){
+			getUsers(searchQuery);
+		}
+	};
 
-			setStudents(studentsData);
-			setAlumni(alumniData);
-			setFaculty(facultyData);
-			setAdmins(adminsData);
-
-			// Combine all into one array
-			const combinedUsers = [...studentsData, ...alumniData, ...facultyData, ...adminsData];
-			setAllUsers(combinedUsers);
-		};
-
-		loadUsers();
-	}, [closeSideNav, getUsersByType]);
-
-	// Filtered users based on search
-	const filteredUsers = allUsers.filter((user) => {
-		const searchLower = search.toLowerCase();
-		return (
-			user.name?.toLowerCase().includes(searchLower) ||
-			user.email?.toLowerCase().includes(searchLower) ||
-			user.organisation?.toLowerCase().includes(searchLower)
-		);
-	});
+	console.log("prof")
 
 	return (
 		<Fragment>
@@ -58,18 +60,21 @@ const Profiles = ({
 				<Spinner />
 			) : (
 				<Fragment>
-					<h1 className="large text-primary" style={{ textAlign: "center" }}>
+					<h1
+						className="large text-primary"
+						style={{ textAlign: "center" }}
+					>
 						Members
 					</h1>
 					<p className="lead" style={{ textAlign: "center" }}>
-						<i className="fab fa-connectdevelop"></i> Browse and connect with members
+						<i className="fab fa-connectdevelop"></i>Browse and
+						connect with members
 					</p>
-
-					{/* Search Bar */}
 					<div className="search-div">
-						<form className="col-12 search-form" onSubmit={(e) => e.preventDefault()}>
+						<form method="get" className="col-12 search-form">
 							<input
 								type="text"
+								
 								name="search"
 								id="search"
 								placeholder="Search Members..."
@@ -81,30 +86,34 @@ const Profiles = ({
 								type="submit"
 								value="Search"
 								className="btn btn-primary col-3 posts-top-item"
+								onClick={handleSubmit}
 							/>
 						</form>
 					</div>
-
-					{/* User type stats */}
-					<div className="user-type-stats" style={{ textAlign: "center" }}>
+					<div
+						className="user-type-stats"
+						style={{ textAlign: "center" }}
+					>
 						<ul className="profile-stats">
-							<UsersByType users={alumni} label={"Alumni"} />
-							<UsersByType users={students} label={"Students"} />
-							<UsersByType users={faculty} label={"Faculty"} />
-							<UsersByType users={admins} label={"Admin"} />
+							<UsersByType users={alumni} label={"Alumni"} key="alumni_count"/>
+							<UsersByType users={students} label={"Students"} key="student_count"/>
+							<UsersByType users={faculty} label={"Faculty"} key="faculty_count"/>
+							<UsersByType users={admins} label={"Admin"} key="admin_count"/>
 						</ul>
 					</div>
-
-					{/* List of filtered users */}
-					<h5 className="row ml-5 pb-2 mt-5" style={{ textAlign: "center" }}>
-						{filteredUsers.length} users found
+					<h5
+						className="row ml-5 pb-2 mt-5"
+						style={{ textAlign: "center" }}
+					>
+						{users && users.length} users found
 					</h5>
-
 					<div className="container profile-page grid-container">
-						{filteredUsers.length > 0 ? (
-							filteredUsers.map((user) => (
-								<UserCard key={user._id} profile={user} />
-							))
+						{users && users.length > 0 ? (
+							users.map((user) => {
+								return (
+									<UserCard key={user._id} profile={user} />
+								);
+							})
 						) : (
 							<h4 style={{ textAlign: "center" }}>
 								No Profiles Found
@@ -119,6 +128,7 @@ const Profiles = ({
 
 Profiles.propTypes = {
 	user: PropTypes.object.isRequired,
+	getUsers: PropTypes.func.isRequired,
 	closeSideNav: PropTypes.func.isRequired,
 	getUsersByType: PropTypes.func.isRequired,
 };
@@ -128,6 +138,7 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, {
+	getUsers,
 	closeSideNav,
 	getUsersByType,
 })(Profiles);
